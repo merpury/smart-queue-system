e51e6c1d4c01754d83b8987a8da53485d4f2fb89
Mercury <parinda.ko@kkumail.com>
2024-10-02 15:23:35 +0700

    update

--- app/add-ons/declarations.d.ts
+++ app/add-ons/declarations.d.ts
@@ -1,7 +1,6 @@
-declare module '*.png' {
-    const value: string;
-    export default value;
-  } 
-//แก้ขัดที่หน้า headerPage.tsx มีปัญหาในการ import image .png
-//ไม่รู้จะเอาไว้หน้าไหน please help...
-  
+// declare module '*.png' {
+//     const value: string;
+//     export default value;
+//   } 
+// //แก้ขัดที่หน้า headerPage.tsx มีปัญหาในการ import image .png
+// //ไม่รู้จะเอาไว้หน้าไหน please help...
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-// import HeaderPage from "./queue/page";
-import home1 from './Home/page'
-import PaymentPage from './payment/page';
-import HeaderPage from './Home/page';
-import AdminPage from "./Admin/page";
+
 import SelectShop from './SelectShop/page';
 
 export default function Home() {
   return (
     <div>
-      <HeaderPage/>
-      {/* <SelectShop/> */}
-      {/* <AdminPage /> */}
+      <SelectShop/>
     </div>
   );
 }
